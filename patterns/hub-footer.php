656a9e8c97689c9fd232a98cd6cf02e097e4d685
ash-uga <aaw97657@uga.edu>
2025-04-04 16:23:10 -0400

    Footer update pt2

--- patterns/hub-footer.php
+++ patterns/hub-footer.php
@@ -11,77 +11,69 @@
 ?>
 
 <!-- wp:group {"metadata":{"name":"hub-footer"},"className":"hub-footer","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"15px","right":"15px"}}},"backgroundColor":"bulldog-red","textColor":"base","layout":{"type":"constrained","contentSize":"80%"}} -->
-<div class="wp-block-group hub-footer has-base-color has-bulldog-red-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--70);padding-right:15px;padding-bottom:var(--wp--preset--spacing--70);padding-left:15px"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"grid","minimumColumnWidth":"20rem"}} -->
-    <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-        <div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
-            <h2 class="wp-block-heading has-large-font-size">About CAES</h2>
-            <!-- /wp:heading -->
+<div class="wp-block-group hub-footer has-base-color has-bulldog-red-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--70);padding-right:15px;padding-bottom:var(--wp--preset--spacing--70);padding-left:15px"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|60"}},"layout":{"type":"grid","minimumColumnWidth":null,"columnCount":2}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">About CAES</h2>
+<!-- /wp:heading -->
 
-            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"grid"}} -->
-            <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|40"}},"border":{"top":{"color":"var:preset|color|base"},"right":{"color":"var:preset|color|base"},"bottom":{"color":"var:preset|color|base"},"left":{"color":"#ffffffb3","width":"1px"}}},"layout":{"type":"constrained"}} -->
-                <div class="wp-block-group" style="border-top-color:var(--wp--preset--color--base);border-right-color:var(--wp--preset--color--base);border-bottom-color:var(--wp--preset--color--base);border-left-color:#ffffffb3;border-left-width:1px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu" data-type="link" data-id="https://www.caes.uga.edu">CAES Home</a></p>
-                    <!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"grid","minimumColumnWidth":"12rem"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|40"}},"border":{"left":{"color":"#ffffffb3","width":"1px"},"top":[],"right":[],"bottom":[]}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-color:#ffffffb3;border-left-width:1px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu" data-type="link" data-id="https://www.caes.uga.edu">CAES Home</a></p>
+<!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/about.html">Overview</a></p>
-                    <!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/about.html">Overview</a></p>
+<!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/about/history.html">History</a></p>
-                    <!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/about/history.html">History</a></p>
+<!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/about/administration.html">Administration</a></p>
-                    <!-- /wp:paragraph -->
-                </div>
-                <!-- /wp:group -->
+<!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/about/administration.html">Administration</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
 
-                <!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|40"}},"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"color":"#ffffffb3","width":"1px"}}},"layout":{"type":"constrained"}} -->
-                <div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-color:#ffffffb3;border-left-width:1px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/about/jobs.html">Jobs</a></p>
-                    <!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|40"}},"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"color":"#ffffffb3","width":"1px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-color:#ffffffb3;border-left-width:1px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/about/jobs.html">Jobs</a></p>
+<!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/about/personnel.html">Personnel Directory</a></p>
-                    <!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/about/personnel.html">Personnel Directory</a></p>
+<!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/privacy.html">Privacy Policy</a></p>
-                    <!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/privacy.html">Privacy Policy</a></p>
+<!-- /wp:paragraph -->
 
-                    <!-- wp:paragraph -->
-                    <p><a href="https://www.caes.uga.edu/accessibility.html">Accessibility Policy</a></p>
-                    <!-- /wp:paragraph -->
-                </div>
-                <!-- /wp:group -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
+<!-- wp:paragraph -->
+<p><a href="https://www.caes.uga.edu/accessibility.html">Accessibility Policy</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
 
-        <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-        <div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
-            <h2 class="wp-block-heading has-large-font-size">Affiliations</h2>
-            <!-- /wp:heading -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Affiliations</h2>
+<!-- /wp:heading -->
 
-            <!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|40"}},"border":{"left":{"color":"#ffffffb3","width":"1px"},"top":{},"right":{},"bottom":{}}},"layout":{"type":"constrained"}} -->
-            <div class="wp-block-group" style="border-left-color:#ffffffb3;border-left-width:1px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph -->
-                <p><a href="https://extension.uga.edu/">UGA Cooperative Extension</a></p>
-                <!-- /wp:paragraph -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"left":"var:preset|spacing|40"}},"border":{"left":{"color":"#ffffffb3","width":"1px"},"top":[],"right":[],"bottom":[]}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="border-left-color:#ffffffb3;border-left-width:1px;padding-left:var(--wp--preset--spacing--40)"><!-- wp:paragraph -->
+<p><a href="https://extension.uga.edu/">UGA Cooperative Extension</a></p>
+<!-- /wp:paragraph -->
 
-                <!-- wp:paragraph -->
-                <p><a href="https://tifton.caes.uga.edu/">Tifton Campus</a></p>
-                <!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><a href="https://tifton.caes.uga.edu/">Tifton Campus</a></p>
+<!-- /wp:paragraph -->
 
-                <!-- wp:paragraph -->
-                <p><a href="https://griffin.uga.edu/">Griffin Campus</a></p>
-                <!-- /wp:paragraph -->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-</div>
+<!-- wp:paragraph -->
+<p><a href="https://griffin.uga.edu/">Griffin Campus</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
