9e72d391ede5c040eb8cb4f849d14cac585b33d3
frankel-chris <ccampbell@frankelagency.com>
2025-04-29 19:10:06 -0400

    Temp Save Post fix

--- functions.php
+++ functions.php
@@ -20,3 +20,45 @@ require get_template_directory() . '/inc/news-support.php';
 require get_template_directory() . '/block-variations/index.php';
 
 
+add_action('init', function () {
+    if (!is_admin() && isset($_GET['trigger_save_posts']) && $_GET['trigger_save_posts'] == 1) {
+        $post_type = 'post';
+
+        $posts = get_posts([
+            'post_type'      => $post_type,
+            'posts_per_page' => -1,
+            'post_status'    => 'any',
+            'fields'         => 'ids',
+        ]);
+
+        foreach ($posts as $post_id) {
+            wp_update_post(['ID' => $post_id]);
+        }
+
+        // Optional: show a simple message in the browser
+        echo count($posts) . " posts for '{$post_type}' were updated.";
+        exit;
+    }
+});
+
+
+add_action('init', function () {
+    if (!is_admin() && isset($_GET['trigger_save_pubs']) && $_GET['trigger_save_pubs'] == 1) {
+        $post_type = 'publications';
+
+        $posts = get_posts([
+            'post_type'      => $post_type,
+            'posts_per_page' => -1,
+            'post_status'    => 'any',
+            'fields'         => 'ids',
+        ]);
+
+        foreach ($posts as $post_id) {
+            wp_update_post(['ID' => $post_id]);
+        }
+
+        // Optional: show a simple message in the browser
+        echo count($posts) . " publications for '{$post_type}' were updated.";
+        exit;
+    }
+});
