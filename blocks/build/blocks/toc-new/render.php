9731aa1b15f7aeb059b55c4f37b2b5d0654b59c4
ash-uga <aaw97657@uga.edu>
2025-05-06 15:11:39 -0400

    Pub single adjustments

--- blocks/build/blocks/toc-new/render.php
+++ blocks/build/blocks/toc-new/render.php
@@ -1,13 +1,19 @@
 <?php
-$title = $attributes['tocHeading'];
+$post_content = get_post_field('post_content', get_the_ID());
 $showSubheadings = $attributes['showSubheadings'];
+$title = $attributes['tocHeading'];
 $listStyle = $attributes['listStyle'];
+
+$pattern = $showSubheadings ? '/<h[2-6][^>]*>.*?<\/h[2-6]>/' : '/<h2[^>]*>.*?<\/h2>/';
+if (!preg_match($pattern, $post_content)) {
+    return '';
+}
 ?>
 <div
     <?php echo get_block_wrapper_attributes(); ?>
-    data-show-subheadings="<?php echo $showSubheadings; ?>"
-    data-list-style="<?php echo $listStyle; ?>"
-    data-title="<?php echo $title; ?>"
+    data-show-subheadings="<?php echo esc_attr($showSubheadings); ?>"
+    data-list-style="<?php echo esc_attr($listStyle); ?>"
+    data-title="<?php echo esc_attr($title); ?>"
 >
-    <h2><?php echo $title; ?></h2>
+    <h2><?php echo esc_html($title); ?></h2>
 </div>
