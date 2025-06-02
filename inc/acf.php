54c9384ff64acd8d88416c98d44fc459556f80f2
ash-uga <aaw97657@uga.edu>
2025-06-02 14:12:52 -0400

    Hide legacy fields on event form

--- inc/acf.php
+++ inc/acf.php
@@ -38,6 +38,36 @@ function caes_hub_acf_show_admin($show) {
 	return (in_array($current_user->user_login, $admins));
 }
 
+// Hide the legacy fields tab for non-admins
+add_filter('acf/prepare_field', 'hide_specific_fields_from_non_admins');
+function hide_specific_fields_from_non_admins($field) {
+    // Only show these fields to admins
+    if (!current_user_can('manage_options')) {
+        $hidden_fields = [
+            'button1_text',
+            'button1_link',
+            'button2_text',
+            'button2_link',
+            'button3_text',
+            'button3_link',
+            'button4_text',
+            'button4_link',
+            'file1',
+            'file1_name',
+            'contact_personnel_id',
+            'submitted_by',
+            'image',
+            'image_caption',
+        ];
+
+        if (in_array($field['name'], $hidden_fields)) {
+            return false;
+        }
+    }
+
+    return $field;
+}
+
 // Include ACF
 include_once( get_template_directory() . '/inc/acf/acf.php' );
 
