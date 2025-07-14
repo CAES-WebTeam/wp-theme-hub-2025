da5708dd94774aff0efb7f8d6f5c7d1e6dcc96ae
ash-uga <aaw97657@uga.edu>
2025-07-14 14:06:35 -0400

    fix to user feed block pt 2

--- blocks/build/blocks/user-feed/index.asset.php
+++ blocks/build/blocks/user-feed/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b5f3dc9818cddb7e40f7');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '289346a400637e9d481f');
