c3bc9ff421d14d5d0eaa801776661dcce8a8cf45
ash-uga <aaw97657@uga.edu>
2025-07-24 10:31:10 -0400

    Fixes to events blocks pt 9

--- blocks/build/blocks/event-details-date-time/render.php
+++ blocks/build/blocks/event-details-date-time/render.php
@@ -12,36 +12,92 @@ $heading = $block['heading'];
 $fontSize = isset($block['headingFontSize']) && !empty($block['headingFontSize']) ? esc_attr($block['headingFontSize']) : '';
 $fontUnit = isset($block['headingFontUnit']) ? esc_attr($block['headingFontUnit']) : 'px';
 
-// Initialize variables
-$date = '';
-$time = '';
-
-// Set Start Date
-if (!empty(get_field('start_date', $post_id))) :
-	$date_object = DateTime::createFromFormat('Ymd', get_field('start_date', $post_id));
-	$formatted_date = $date_object ? $date_object->format('F j, Y') : 'Invalid date format';
-	$date = $formatted_date;
-endif;
-
-// Set End Date
-if (!empty(get_field('end_date', $post_id))) :
-	$date_object = DateTime::createFromFormat('Ymd', get_field('end_date', $post_id));
-	$formatted_date = $date_object ? $date_object->format('F j, Y') : 'Invalid date format';
-	$date = $date . '-' . $formatted_date;
-endif;
-
-// Set Start Time
-if (!empty(get_field('start_time', $post_id))) :
-	$time = get_field('start_time', $post_id);
-endif;
-
-// Set End Time
-if (!empty(get_field('end_time', $post_id))) :
-	$time = $time . '-' . get_field('end_time', $post_id);
-endif;
-
 // Generate inline style if font size is set
 $style = $fontSize ? ' style="font-size: ' . $fontSize . $fontUnit . ';"' : '';
+
+// Get date type
+$date_type = get_field('event_date_type', $post_id);
+
+// Initialize variables
+$date_output = '';
+
+if ($date_type === 'single') {
+    // Handle single event
+    $date = '';
+    $time = '';
+    
+    // Set Start Date
+    if (!empty(get_field('start_date', $post_id))) :
+        $date_object = DateTime::createFromFormat('Ymd', get_field('start_date', $post_id));
+        $formatted_date = $date_object ? $date_object->format('F j, Y') : 'Invalid date format';
+        $date = $formatted_date;
+    endif;
+
+    // Set End Date
+    if (!empty(get_field('end_date', $post_id))) :
+        $date_object = DateTime::createFromFormat('Ymd', get_field('end_date', $post_id));
+        $formatted_date = $date_object ? $date_object->format('F j, Y') : 'Invalid date format';
+        $date = $date . '-' . $formatted_date;
+    endif;
+
+    // Set Start Time
+    if (!empty(get_field('start_time', $post_id))) :
+        $time = get_field('start_time', $post_id);
+    endif;
+
+    // Set End Time
+    if (!empty(get_field('end_time', $post_id))) :
+        $time = $time . '-' . get_field('end_time', $post_id);
+    endif;
+    
+    // Build single date output
+    if ($showDate && !empty($date)) {
+        $date_output .= $date;
+    }
+    if ($showTime && !empty($time)) {
+        if ($showDate) $date_output .= '<br />';
+        $date_output .= $time;
+    }
+    
+} elseif ($date_type === 'multi') {
+    // Handle multiday event
+    $multi_dates = get_field('date_and_time', $post_id);
+    
+    if (!empty($multi_dates) && is_array($multi_dates)) {
+        foreach ($multi_dates as $date_entry) {
+            $session_date = '';
+            $session_time = '';
+            
+            // Format date
+            if (!empty($date_entry['start_date_copy'])) {
+                $date_object = DateTime::createFromFormat('Ymd', $date_entry['start_date_copy']);
+                $session_date = $date_object ? $date_object->format('F j, Y') : 'Invalid date';
+            }
+            
+            // Format time
+            if (!empty($date_entry['start_time_copy'])) {
+                $session_time = $date_entry['start_time_copy'];
+                if (!empty($date_entry['end_time_copy'])) {
+                    $session_time .= '-' . $date_entry['end_time_copy'];
+                }
+            }
+            
+            // Build this session's output
+            $session_output = '';
+            if ($showDate && !empty($session_date)) {
+                $session_output .= $session_date;
+            }
+            if ($showTime && !empty($session_time)) {
+                if ($showDate) $session_output .= '<br />';
+                $session_output .= $session_time;
+            }
+            
+            if (!empty($session_output)) {
+                $date_output .= '<p>' . $session_output . '</p>';
+            }
+        }
+    }
+}
 ?>
 
 <div <?php echo $attrs; ?>>
@@ -52,25 +108,31 @@ $style = $fontSize ? ' style="font-size: ' . $fontSize . $fontUnit . ';"' : '';
             </p>
         <?php elseif ($dateAsSnippet): ?>
             <h3 class="event-details-title"<?php echo $style; ?>>
-                <?php echo !empty($date) ? esc_html($date) : esc_html__('No date available', 'caes-hub'); ?>
+                <?php 
+                // For snippet mode, show "Multiple dates" for multi events
+                if ($date_type === 'multi') {
+                    echo esc_html__('Multiple dates', 'caes-hub');
+                } else {
+                    echo !empty($date) ? esc_html($date) : esc_html__('No date available', 'caes-hub');
+                }
+                ?>
             </h3>
         <?php else: ?>
             <h3 class="event-details-title"<?php echo $style; ?>>
-                <?php echo esc_html__('Date', 'caes-hub') . ($showTime ? esc_html__(' & Time', 'caes-hub') : ''); ?>
+                <?php 
+                if ($date_type === 'multi') {
+                    echo esc_html__('Dates', 'caes-hub');
+                } else {
+                    echo esc_html__('Date', 'caes-hub') . ($showTime ? esc_html__(' & Time', 'caes-hub') : '');
+                }
+                ?>
             </h3>
         <?php endif; ?>
     <?php endif; ?>
 
-    <?php if (!$dateAsSnippet && ($showDate || $showTime)): ?>
+    <?php if (!$dateAsSnippet && ($showDate || $showTime) && !empty($date_output)): ?>
         <div class="event-details-content">
-            <?php if ($showDate && !empty($date)): ?>
-                <?php echo esc_html($date); ?>
-            <?php endif; ?>
-
-            <?php if ($showTime && !empty($time)): ?>
-                <?php if ($showDate): ?><br /><?php endif; ?>
-                <?php echo esc_html($time); ?>
-            <?php endif; ?>
+            <?php echo $date_output; ?>
         </div>
     <?php endif; ?>
 </div>
--- patterns/event-grid-1.php
+++ patterns/event-grid-1.php
@@ -16,27 +16,22 @@
 
 <!-- wp:query {"queryId":5,"query":{"postType":"events","offset":0,"filterByDate":true,"perPage":6},"namespace":"upcoming-events","className":"caes-hub-post-list-grid"} -->
 <div class="wp-block-query caes-hub-post-list-grid"><!-- wp:post-template {"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"24rem"}} -->
-    <!-- wp:group {"metadata":{"name":"caes-hub-post-list-grid-item"},"className":"caes-hub-post-list-grid-item","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-    <div class="wp-block-group caes-hub-post-list-grid-item"><!-- wp:caes-hub/event-details-featured-image {"className":"caes-hub-post-list-img-container"} /-->
+<!-- wp:group {"metadata":{"name":"caes-hub-post-list-grid-item"},"className":"caes-hub-post-list-grid-item","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+<div class="wp-block-group caes-hub-post-list-grid-item"><!-- wp:caes-hub/event-details-featured-image {"className":"caes-hub-post-list-img-container"} /-->
 
-        <!-- wp:group {"metadata":{"name":"caes-hub-post-list-grid-info"},"className":"caes-hub-post-list-grid-info","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","verticalAlignment":"space-between"}} -->
-        <div class="wp-block-group caes-hub-post-list-grid-info has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
-            <div class="wp-block-group"><!-- wp:post-title {"isLink":true,"className":"caes-hub-post-list-grid-title","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontSize":"medium"} /-->
+<!-- wp:group {"metadata":{"name":"caes-hub-post-list-grid-info"},"className":"caes-hub-post-list-grid-info","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"backgroundColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"left","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group caes-hub-post-list-grid-info has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:post-title {"isLink":true,"className":"caes-hub-post-list-grid-title","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","fontSize":"medium"} /-->
 
-                <!-- wp:caes-hub/event-details-date-time {"heading":false} /-->
-            </div>
-            <!-- /wp:group -->
-        </div>
-        <!-- /wp:group -->
+<!-- wp:caes-hub/event-details-date-time {"dateAsSnippet":true,"headingFontSize":"1.1"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
 
-        <!-- wp:group {"metadata":{"name":"caes-hub-content-actions"},"className":"caes-hub-content-actions ","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-        <div class="wp-block-group caes-hub-content-actions"><!-- wp:caes-hub/action-save /-->
+<!-- wp:group {"metadata":{"name":"caes-hub-content-actions"},"className":"caes-hub-content-actions ","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group caes-hub-content-actions"><!-- wp:caes-hub/action-save /-->
 
-            <!-- wp:caes-hub/action-share /-->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
-    <!-- /wp:post-template -->
-</div>
+<!-- wp:caes-hub/action-share /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
 <!-- /wp:query -->
