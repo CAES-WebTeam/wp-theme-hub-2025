f8ec1525f9a4d015922fc986971f2a4c95ca9c78
jdk48542 <jdk48542@uga.edu>
2025-07-25 15:24:03 -0400

    Tweaking WordPress AJAX

--- inc/pub-main-import.php
+++ inc/pub-main-import.php
@@ -310,7 +310,7 @@ function publication_api_tool_compare_publications() {
         $args = array(
             'post_type'      => 'publications', // Assuming 'publication' is the correct post type
             'posts_per_page' => -1,          // Get all publications
-            'post_status'    => 'published',   // Only published ones
+            'post_status'    => 'publish',   // Only published ones
             // 'fields'         => 'titles',    // This only returns an array of WP_Post objects with only title property.
                                              // It's generally safer to get the full post object and then extract the title.
         );
