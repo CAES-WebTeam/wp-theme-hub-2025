a920b885c69a08e3e99123913219b5d6826a5da4
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-08-07 08:55:39 -0400

    Fix to hand picked posts search for posts

--- blocks/build/blocks/hand-picked-post/index.asset.php
+++ blocks/build/blocks/hand-picked-post/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bff83ec65ad187429eda');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '50af0810d0c18b5bcab4');
