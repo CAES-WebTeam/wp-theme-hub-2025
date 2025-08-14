c92788896ba8ca30d8cd7d32cde35812ee7da3ae
Ashley A Williams <aaw97657@s172-20-180-h51.paws.uga.edu>
2025-08-14 11:12:55 -0400

    Adding topic rss feeds pt 2

--- inc/rss-support.php
+++ inc/rss-support.php
@@ -1,11 +1,50 @@
 <?php
 
+// Disable Yoast RSS feed modifications
+add_filter('wpseo_enable_rss_modification', '__return_false');
+add_filter('wpseo_remove_reply_to_com', '__return_false');
+
 // Custom RSS2 feed with ACF authors
 remove_all_actions('do_feed_rss2');
 function create_custom_rss2_feed() {
-    load_template(get_template_directory() . '/inc/rss-template.php');
+    load_template(get_template_directory() . '/inc/feed-rss2.php');
 }
 add_action('do_feed_rss2', 'create_custom_rss2_feed', 10, 1);
+
+// Filter taxonomy feeds by post type based on URL
+function filter_taxonomy_feeds_by_post_type($query) {
+    if (!is_feed() || !$query->is_main_query()) {
+        return;
+    }
+    
+    // Only run on taxonomy queries
+    if (!$query->is_tax()) {
+        return;
+    }
+    
+    // Get the current URL path
+    $request_uri = $_SERVER['REQUEST_URI'];
+    
+    // Determine post type from URL
+    $post_type = null;
+    if (strpos($request_uri, '/publications/topic/') !== false) {
+        $post_type = 'publications';
+    } elseif (strpos($request_uri, '/news/topic/') !== false) {
+        $post_type = 'post';
+    } elseif (strpos($request_uri, '/shorthand-story/topic/') !== false) {
+        $post_type = 'shorthand_story';
+    } elseif (strpos($request_uri, '/events/topic/') !== false) {
+        $post_type = 'events';
+    }
+    
+    // Filter the query if we found a post type
+    if ($post_type) {
+        $query->set('post_type', $post_type);
+        // Ensure we get the right posts
+        $query->set('posts_per_page', get_option('posts_per_rss', 10));
+    }
+}
+add_action('pre_get_posts', 'filter_taxonomy_feeds_by_post_type');
 
 // Helper function to get ACF authors (for use in custom feed template)
 function get_acf_authors_for_feed($post_id) {
