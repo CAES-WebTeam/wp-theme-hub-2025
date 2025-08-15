13b6194a7b8a997cdb82414fc387cb7aa1d34b1f
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-08-15 07:19:33 -0400

    incorporate shorthand post types into post feeds pt 11

--- block-variations/index.php
+++ block-variations/index.php
@@ -356,3 +356,27 @@ function display_debug_stories_feed()
     }
 }
 add_action('wp_footer', 'display_debug_stories_feed');
+
+if ('stories-feed' === $namespace) {
+    // Check if shorthand_story posts exist for this author
+    $test_query = new WP_Query([
+        'post_type' => 'shorthand_story',
+        'meta_query' => [
+            'relation' => 'OR',
+            [
+                'key' => 'all_expert_ids',
+                'value' => 'i:1561;',
+                'compare' => 'LIKE'
+            ],
+            [
+                'key' => 'all_author_ids', 
+                'value' => 'i:1561;',
+                'compare' => 'LIKE'
+            ]
+        ],
+        'posts_per_page' => -1
+    ]);
+    
+    $debug_stories_feed[] = "Found " . $test_query->found_posts . " shorthand_story posts for this author";
+    wp_reset_postdata();
+}
