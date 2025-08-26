e037bb74ffd7a288d9667d40e7d3ad2498b4d2dd
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-08-26 09:50:50 -0400

    Update to new post default content

--- inc/news-support.php
+++ inc/news-support.php
@@ -71,6 +71,20 @@ function story_default_content($content, $post)
 
 <!-- wp:paragraph -->
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam faucibus nibh ex, eu cursus orci faucibus quis. Nunc ut feugiat dui. Praesent congue sit amet felis in blandit. In tristique odio ut nisi auctor consectetur. Nunc nunc sapien, luctus et orci a, imperdiet aliquam nisi. Integer efficitur lacus at purus molestie, in auctor nunc fermentum. Nulla pharetra felis sed tincidunt pharetra.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"metadata":{"name":"Related Content","categories":["story_feeds","pub_feeds"],"patternName":"caes-hub/related-content"},"className":"is-style-caes-hub-align-left-40","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|30","margin":{"bottom":"0"}},"border":{"left":{"color":"var:preset|color|hedges","width":"5px"},"top":[],"right":[],"bottom":[]}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-caes-hub-align-left-40" style="border-left-color:var(--wp--preset--color--hedges);border-left-width:5px;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--50)"><!-- wp:heading {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"regular","fontFamily":"oswald"} -->
+<h2 class="wp-block-heading has-oswald-font-family has-regular-font-size" style="text-transform:uppercase">Related Content</h2>
+<!-- /wp:heading -->
+
+<!-- wp:caes-hub/hand-picked-post {"postType":["post","publications","shorthand_story"],"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<!-- wp:post-title {"level":3,"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"textDecoration":"underline"},"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"0"}}},"fontSize":"regular"} /-->
+<!-- /wp:caes-hub/hand-picked-post --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam faucibus nibh ex, eu cursus orci faucibus quis. Nunc ut feugiat dui. Praesent congue sit amet felis in blandit. In tristique odio ut nisi auctor consectetur. Nunc nunc sapien, luctus et orci a, imperdiet aliquam nisi. Integer efficitur lacus at purus molestie, in auctor nunc fermentum. Nulla pharetra felis sed tincidunt pharetra.</p>
 <!-- /wp:paragraph -->';
 
     return $default_content . "\n\n" . $content;
