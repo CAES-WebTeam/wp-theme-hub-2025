264f65eb91abff04f81fa955460e2afd678826be
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-08-27 10:36:23 -0400

    fix tables in pdf generation pt 25

--- inc/publications-pdf/pdf-admin.php
+++ inc/publications-pdf/pdf-admin.php
@@ -544,6 +544,7 @@ function fr2025_ajax_get_publications_table() {
                 $actions .= '<button class="button button-small button-primary regenerate-pdf-btn" data-post-id="' . $pub->ID . '" data-post-title="' . esc_attr($pub->post_title) . '">' . $button_text . '</button>';
             }
             
+            $actions .= '<br/>';
             $actions .= '<a href="' . esc_url(get_edit_post_link($pub->ID)) . '" style="margin-left: 5px;" title="Edit Publication">Edit</a>';
             $actions .= '<a href="' . esc_url(get_permalink($pub->ID)) . '" target="_blank" style="margin-left: 5px;" title="View Publication on site">View</a> ';
             
