1b5df054cd1fb80c52905353444c1af61a55097a
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-09-10 16:09:53 -0400

    Adding filtered topics block pt 8

--- blocks/build/blocks/filtered-topics/index.asset.php
+++ blocks/build/blocks/filtered-topics/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'd5976ede3fb34980ee0b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '6a1578b807abb7e4abbe');
