2182b87e78bdabfaa9cd51835706486999797cf3
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-09-11 10:33:54 -0400

    Removing extra tools pt 2

--- inc/caes-tools.php
+++ inc/caes-tools.php
@@ -21,27 +21,7 @@ function caes_tools_main_page()
     ?>
     <div class="wrap">
         <h1>CAES Tools</h1>
-        <p>Welcome to the CAES Tools dashboard. Please select a tool from the submenu.</p>
-        
-        <div class="card">
-            <h2>Tools for publications and stories</h2>
-            <ul>
-                <li><a href="<?php echo admin_url('admin.php?page=user-data-management'); ?>">User Data Management</a></li>
-                <li><a href="<?php echo admin_url('admin.php?page=link-content-admin'); ?>">Link Writers, Experts, and Publication Authors</a></li>
-            </ul>
-            <h2>Tools for publications</h2>
-            <ul>
-                <li><a href="<?php echo admin_url('admin.php?page=publication-api-tool'); ?>">Publication Import Tool</a></li>
-                <li><a href="<?php echo admin_url('admin.php?page=update-publication-history'); ?>">Update Publication History</a></li>
-            </ul>
-            <h2>Tools for stories</h2>
-            <ul>
-                <li><a href="<?php echo admin_url('admin.php?page=story-meta-association-tools'); ?>">Story Meta Association Tools</a></li>
-                <li><a href="<?php echo admin_url('admin.php?page=release-date-migration'); ?>">Story Release Date Migration</a></li>
-                <li><a href="<?php echo admin_url('admin.php?page=clear-release-date'); ?>">Story Clear Release Date Field</a></li>
-                <li><a href="<?php echo admin_url('admin.php?page=duplicate-post-checker'); ?>">Story Duplicate Checker</a></li>
-            </ul>
-        </div>
+        <p>Welcome to the CAES Tools dashboard. Please select a tool from the submenu on the left.</p>
     </div>
     <?php
 }
