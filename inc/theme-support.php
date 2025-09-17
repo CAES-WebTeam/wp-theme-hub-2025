0fa38a143028880652e93d84ceb6234c0bb91c21
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-09-17 11:44:21 -0400

    Adding favicons for theme pt 2

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -843,8 +843,8 @@ function my_theme_add_favicon_final() {
     // -- Standard & PNG Favicons --
     echo '';
     echo '<link rel="icon" type="image/x-icon" href="' . esc_url($favicon_path . 'favicon.ico') . '">';
-    echo '<link rel="icon" type="image/png" sizes="16x16" href="' . esc_url($favicon_path . 'favicon-16x16.png') . '">';
-    echo '<link rel="icon" type="image/png" sizes="32x32" href="' . esc_url($favicon_path . 'favicon-32x32.png') . '">';
+    // echo '<link rel="icon" type="image/png" sizes="16x16" href="' . esc_url($favicon_path . 'favicon-16x16.png') . '">';
+    // echo '<link rel="icon" type="image/png" sizes="32x32" href="' . esc_url($favicon_path . 'favicon-32x32.png') . '">';
     echo '<link rel="icon" type="image/png" sizes="96x96" href="' . esc_url($favicon_path . 'favicon-96x96.png') . '">';
     echo '<link rel="icon" type="image/png" sizes="128x128" href="' . esc_url($favicon_path . 'favicon-128.png') . '">';
     echo '<link rel="icon" type="image/png" sizes="196x196" href="' . esc_url($favicon_path . 'favicon-196x196.png') . '">';
