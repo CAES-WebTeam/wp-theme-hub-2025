8b37b971a23be84d70e2ad2789ea68f1a6c44ca0
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-11-17 13:30:44 -0500

    Fix to hide legacy gallery again?

--- blocks/build/blocks/legacy-gallery/index.asset.php
+++ blocks/build/blocks/legacy-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '04587966a7b3bbd1d72a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2a60b79c163f75180d28');
