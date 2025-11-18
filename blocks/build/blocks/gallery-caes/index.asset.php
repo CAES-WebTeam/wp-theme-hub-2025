fe7022e742ec3edf3be74268dd27ee39d2f7c37d
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-11-18 15:12:56 -0500

    Here comes CAES gallery pt 15

--- blocks/build/blocks/gallery-caes/index.asset.php
+++ blocks/build/blocks/gallery-caes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5aa8a805559fb637b514');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9b939b2fbeae8f978e02');
