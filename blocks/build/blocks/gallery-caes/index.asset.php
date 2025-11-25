4a8532b9dacbb891177377b64f0816446e76fafd
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-11-25 14:24:16 -0500

    Make gallery look more like front end pt 4

--- blocks/build/blocks/gallery-caes/index.asset.php
+++ blocks/build/blocks/gallery-caes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '70436bb68b3e6ec0fcbd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8cb563157516d81c93c8');
