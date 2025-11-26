4d72fc87350ef8b9ea3ea623d6725fdac704a833
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-11-26 09:03:43 -0500

    Trying the option of captions displaying on gallery pt 2

--- blocks/build/blocks/gallery-caes/index.asset.php
+++ blocks/build/blocks/gallery-caes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b7176e1fa35a1e7efbb5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '82ec10edc5308f685448');
