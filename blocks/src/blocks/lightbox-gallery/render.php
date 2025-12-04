ad563721d9de6c461c8cef850ba3d168d86b56dc
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2025-12-04 14:10:14 -0500

    Large size for image galleries pt 4

--- blocks/src/blocks/lightbox-gallery/render.php
+++ blocks/src/blocks/lightbox-gallery/render.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Server-side rendering for the lightbox gallery block
  */
@@ -25,27 +24,27 @@ $placeholder = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAA
     <div class="gallery-trigger">
         <!-- Hidden gallery links for Parvus -->
         <div class="parvus-gallery" style="display: none;" aria-hidden="true">
-            <?php foreach ($images as $image):
-                $medium_url = $image['sizes']['medium_large']['url'] ?? $image['sizes']['large']['url'] ?? $image['url'];
-                $large_url = $image['sizes']['large']['url'] ?? $image['url'];
-                $full_url = $image['url'];
+            <?php foreach ($images as $image): 
+                $medium_url = set_url_scheme($image['sizes']['medium_large']['url'] ?? $image['sizes']['large']['url'] ?? $image['url']);
+                $large_url = set_url_scheme($image['sizes']['large']['url'] ?? $image['url']);
+                $full_url = set_url_scheme($image['url']);
             ?>
-                <a href="<?php echo esc_url($full_url); ?>"
-                    class="lightbox"
-                    data-srcset="<?php echo esc_url($medium_url); ?> 768w, <?php echo esc_url($large_url); ?> 1024w, <?php echo esc_url($full_url); ?> 1600w"
-                    data-sizes="(max-width: 75em) 100vw, 75em"
-                    <?php if (!empty($image['caption'])): ?>
-                    data-caption="<?php echo esc_attr($image['caption']); ?>"
-                    <?php endif; ?>>
-                    <img src="<?php echo esc_attr($placeholder); ?>"
-                        alt="<?php echo esc_attr($image['alt'] ?? ''); ?>">
+                <a href="<?php echo esc_url($full_url); ?>" 
+                   class="lightbox"
+                   data-srcset="<?php echo esc_url($medium_url); ?> 768w, <?php echo esc_url($large_url); ?> 1024w, <?php echo esc_url($full_url); ?> 1600w"
+                   data-sizes="(max-width: 75em) 100vw, 75em"
+                   <?php if (!empty($image['caption'])): ?>
+                   data-caption="<?php echo esc_attr($image['caption']); ?>"
+                   <?php endif; ?>>
+                    <img src="<?php echo esc_attr($placeholder); ?>" 
+                         alt="<?php echo esc_attr($image['alt'] ?? ''); ?>">
                 </a>
             <?php endforeach; ?>
         </div>
-
+        
         <!-- Visible trigger -->
         <div class="gallery-trigger-visible">
-            <?php $trigger_url = $images[0]['sizes']['large']['url'] ?? $images[0]['url']; ?>
+            <?php $trigger_url = set_url_scheme($images[0]['sizes']['large']['url'] ?? $images[0]['url']); ?>
             <img
                 src="<?php echo esc_url($trigger_url); ?>"
                 alt="<?php echo esc_attr($images[0]['alt'] ?? ''); ?>"
