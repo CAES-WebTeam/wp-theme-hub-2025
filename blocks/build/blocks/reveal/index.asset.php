b8d68ab619381e8dd7918d7a49f59d47959bc0a3
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-01-28 15:59:50 -0500

    add missing transitions pt 8

--- blocks/build/blocks/reveal/index.asset.php
+++ blocks/build/blocks/reveal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '38989822fdc823972f02');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a4fbb7730c3626fc9d0f');
