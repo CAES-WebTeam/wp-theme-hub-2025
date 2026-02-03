b1f70f46f143c74370eae45e32bd105a7ae12251
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-03 15:33:35 -0500

    component popover modal higher pt 2

--- blocks/build/blocks/reveal/index.asset.php
+++ blocks/build/blocks/reveal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a92b850a1fe12f0e2c81');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aea87580cf942bbe7eec');
