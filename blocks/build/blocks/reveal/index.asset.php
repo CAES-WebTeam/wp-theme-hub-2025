eb1376a7b8ce88a1d8798f825b9922088ae11ef4
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-06 09:08:57 -0500

    width alignment set up

--- blocks/build/blocks/reveal/index.asset.php
+++ blocks/build/blocks/reveal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a12ecfb31692eea98ed4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '63b5f8eca73b2f0b460a');
