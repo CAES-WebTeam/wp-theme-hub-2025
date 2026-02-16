cb8bd6fe053d696da7e1273c58fdceb29e930609
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-16 16:28:06 -0500

    Remove motion scroll and fix inserter appearance on reveal

--- blocks/build/blocks/reveal/index.asset.php
+++ blocks/build/blocks/reveal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '60e718d74168d4790bb0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e748420d4295dfea3233');
