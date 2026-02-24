f293b7d43ff0cd32c718a1164a41b1269c45e92c
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-24 14:05:17 -0500

    Fixed editor styles

--- blocks/build/blocks/motion-scroll/index.asset.php
+++ blocks/build/blocks/motion-scroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '45e45b7775de53136dcf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fb2e47b70f530ab98889');
