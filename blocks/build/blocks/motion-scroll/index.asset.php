550bd4e94670e0311082a6c3195dca65646bee1f
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-25 08:25:13 -0500

    Account for portrait sized images

--- blocks/build/blocks/motion-scroll/index.asset.php
+++ blocks/build/blocks/motion-scroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '93e2cd7cf47997965595');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'adf3430decb415eda372');
