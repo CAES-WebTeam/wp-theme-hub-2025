0c2dcce4466c2bbef23ac5945d529e62eb189f71
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-25 09:25:08 -0500

    Motion Scroll improevements pt 2

--- blocks/build/blocks/motion-scroll/index.asset.php
+++ blocks/build/blocks/motion-scroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'adf3430decb415eda372');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cf9cec1c543f448c6a95');
