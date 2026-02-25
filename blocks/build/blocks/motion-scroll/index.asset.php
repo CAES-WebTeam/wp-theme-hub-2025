ea5157b7664fd5a1e56c227586b5b9af54d008b4
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-02-25 11:26:32 -0500

    Fix editor width on block

--- blocks/build/blocks/motion-scroll/index.asset.php
+++ blocks/build/blocks/motion-scroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'eb4376f3e060b92c3500');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aa904a69c8f16c4114fc');
