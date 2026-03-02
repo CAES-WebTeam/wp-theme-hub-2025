90a27e3764e3d2075bb79ef3cb03bef61812daf1
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-03-02 16:22:45 -0500

    Fix for caption color in editor pt 2

--- blocks/build/blocks/motion-scroll/index.asset.php
+++ blocks/build/blocks/motion-scroll/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7fba1ee30e75d3fa34f9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7f036fe4581aee6c15da');
