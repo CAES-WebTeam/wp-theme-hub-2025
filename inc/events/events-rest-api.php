383d51c9ef44461995c35da47e847a0915d78dfe
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-03-11 08:41:37 -0400

    Fix "headers already sent" warnings in events REST API CORS handler

--- inc/events/events-rest-api.php
+++ inc/events/events-rest-api.php
@@ -230,9 +230,11 @@ add_action('rest_api_init', 'add_cors_http_header');
 function add_cors_http_header() {
     remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
     add_filter('rest_pre_serve_request', function($value) {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET');
-        header('Access-Control-Allow-Credentials: true');
+        if (!headers_sent()) {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: GET');
+            header('Access-Control-Allow-Credentials: true');
+        }
         return $value;
     });
 }
