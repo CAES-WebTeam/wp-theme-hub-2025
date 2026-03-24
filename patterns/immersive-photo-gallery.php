0eb6b6cdf76b2c96e7bacd326ccb9fb7f25e97f7
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-03-24 10:31:11 -0400

    Fix to immersive photo gallery

--- patterns/immersive-photo-gallery.php
+++ patterns/immersive-photo-gallery.php
@@ -12,18 +12,14 @@ $placeholder_bg = get_theme_file_uri( 'assets/images/hotdog.jpg' );
 $pullquote_bg   = get_theme_file_uri( 'assets/images/Fly-Background.jpg' );
 ?>
 
-<!-- wp:cover {"url":"<?php echo esc_url( $placeholder_bg ); ?>","dimRatio":50,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":300,"minHeightUnit":"px","contentPosition":"center center","sizeSlug":"large","align":"full","layout":{"type":"constrained"}} -->
+<!-- wp:cover {"url":"<?php echo esc_url( $placeholder_bg ); ?>","dimRatio":50,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":300,"minHeightUnit":"px","contentPosition":"center center","sizeSlug":"large","metadata":{"categories":["immersive"],"patternName":"caes-theme/immersive-cover","name":"Immersive Cover"},"align":"full","layout":{"type":"constrained"}} -->
 <div class="wp-block-cover alignfull" style="min-height:300px"><img class="wp-block-cover__image-background size-large" alt="" src="<?php echo esc_url( $placeholder_bg ); ?>" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:post-title {"level":1,"className":"is-style-caes-hub-full-underline","style":{"spacing":{"margin":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"}},"textColor":"base","fontFamily":"oswald"} /-->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
-<p class="has-base-color has-text-color has-link-color"><em>Written by</em><br><em><strong>Author</strong></em></p>
-<!-- /wp:paragraph -->
+<!-- wp:caes-hub/pub-details-authors {"displayVersion":"names-only","showHeading":false,"customHeading":"Written by","snippetPrefix":"Written by","snippetPrefixShown":true,"grid":false,"className":"is-style-default","style":{"typography":{"lineHeight":"1","fontStyle":"italic","fontWeight":"400"}}} /-->
 
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
-<p class="has-base-color has-text-color has-link-color"><em>Illustrated by<br><strong>Artist</strong></em></p>
-<!-- /wp:paragraph --></div>
+<!-- wp:caes-hub/pub-details-authors {"displayVersion":"names-only","showHeading":false,"customHeading":"Written by","type":"artists","snippetPrefix":"Illustrated by","snippetPrefixShown":true,"grid":false,"className":"is-style-default","style":{"typography":{"lineHeight":"1","fontStyle":"italic","fontWeight":"400"}}} /--></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
