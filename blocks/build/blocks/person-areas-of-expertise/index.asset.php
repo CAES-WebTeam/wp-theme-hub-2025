bd019be077f4772d09302df89a04cda3dadb528e
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-04-02 10:53:18 -0400

    Fixing block gap by adjusting heading margin

--- blocks/build/blocks/person-areas-of-expertise/index.asset.php
+++ blocks/build/blocks/person-areas-of-expertise/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a98fac679e3cecfc198b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '474e5ad74041e97a4d90');
