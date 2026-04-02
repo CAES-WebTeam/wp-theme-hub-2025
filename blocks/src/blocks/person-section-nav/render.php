07f53f700facfb3f35e9409907106b8e41f49d2c
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-04-02 16:04:32 -0400

    New person-section-nav block pt 2

--- blocks/src/blocks/person-section-nav/render.php
+++ blocks/src/blocks/person-section-nav/render.php
@@ -1,17 +1,44 @@
 <?php
+$author_id = isset($GLOBALS['caes_current_user_id']) ? $GLOBALS['caes_current_user_id'] : get_queried_object_id();
+$person_post_id = function_exists('resolve_person_post_id') ? resolve_person_post_id($author_id) : null;
+
+// Check data availability per section
+$has = [
+    'showAreasOfExpertise' => false,
+    'showAbout'            => false,
+    'showEducation'        => false,
+    'showAwards'           => false,
+    'showCourses'          => false,
+    'showScholarlyWorks'   => false,
+];
+
+if ($person_post_id) {
+    $term_ids = get_field('elements_areas_of_expertise', $person_post_id);
+    $has['showAreasOfExpertise'] = !empty($term_ids);
+
+    $has['showAbout'] = !empty(get_field('elements_overview', $person_post_id));
+
+    // Education: no block yet, check for a stored field or just allow toggle to control it
+    $has['showEducation'] = true;
+
+    $has['showAwards']       = !empty(get_field('elements_distinctions', $person_post_id));
+    $has['showCourses']      = !empty(get_field('elements_courses_taught', $person_post_id));
+    $has['showScholarlyWorks'] = !empty(get_field('elements_scholarly_works', $person_post_id));
+}
+
 $sections = [
-    'showAreasOfExpertise' => [ 'label' => 'Areas of expertise',  'anchor' => 'areas-of-expertise' ],
-    'showAbout'            => [ 'label' => 'About',               'anchor' => 'about' ],
-    'showEducation'        => [ 'label' => 'Education',           'anchor' => 'education' ],
-    'showAwards'           => [ 'label' => 'Awards and honors',   'anchor' => 'awards-and-honors' ],
-    'showCourses'          => [ 'label' => 'Courses',             'anchor' => 'courses' ],
-    'showScholarlyWorks'   => [ 'label' => 'Scholarly works',     'anchor' => 'scholarly-works' ],
+    'showAreasOfExpertise' => [ 'label' => 'Areas of expertise', 'anchor' => 'areas-of-expertise' ],
+    'showAbout'            => [ 'label' => 'About',              'anchor' => 'about' ],
+    'showEducation'        => [ 'label' => 'Education',          'anchor' => 'education' ],
+    'showAwards'           => [ 'label' => 'Awards and honors',  'anchor' => 'awards-and-honors' ],
+    'showCourses'          => [ 'label' => 'Courses',            'anchor' => 'courses' ],
+    'showScholarlyWorks'   => [ 'label' => 'Scholarly works',    'anchor' => 'scholarly-works' ],
 ];
 
 $links = [];
 foreach ($sections as $attr => $section) {
     $enabled = isset($block[$attr]) ? (bool) $block[$attr] : true;
-    if ($enabled) {
+    if ($enabled && $has[$attr]) {
         $links[] = $section;
     }
 }
