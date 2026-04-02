386744214414bf0ea9a95b8925a5d45f411c9dd7
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-04-02 10:08:12 -0400

    Style adjustments

--- blocks/src/blocks/person-areas-of-expertise/edit.js
+++ blocks/src/blocks/person-areas-of-expertise/edit.js
@@ -1,13 +1,31 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import {
+    useBlockProps,
+    InspectorControls,
+    BlockControls,
+    HeadingLevelDropdown,
+    FontSizePicker,
+    useSettings,
+} from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from '@wordpress/components';
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
-    const { linkTerms, showHeading } = attributes;
+    const { linkTerms, showHeading, headingLevel, termFontSize } = attributes;
+    const [ fontSizes ] = useSettings( 'typography.fontSizes' );
+
+    const TagName = `h${headingLevel}`;
 
     return (
         <>
+            {showHeading && (
+                <BlockControls group="block">
+                    <HeadingLevelDropdown
+                        value={headingLevel}
+                        onChange={(value) => setAttributes({ headingLevel: value })}
+                    />
+                </BlockControls>
+            )}
             <InspectorControls>
                 <PanelBody title={__('Areas of Expertise Settings', 'caes-hub')}>
                     <ToggleControl
@@ -22,11 +40,34 @@ export default function Edit({ attributes, setAttributes }) {
                     />
                 </PanelBody>
             </InspectorControls>
+            <InspectorControls group="typography">
+                <PanelBody title={__('Term size', 'caes-hub')}>
+                    <FontSizePicker
+                        fontSizes={fontSizes}
+                        value={termFontSize}
+                        onChange={(value) => setAttributes({ termFontSize: value || '1.3rem' })}
+                        withReset
+                        size="__unstable-large"
+                    />
+                </PanelBody>
+            </InspectorControls>
             <div {...useBlockProps({ className: 'person-areas-of-expertise' })}>
-                {showHeading && <h2 className="person-areas-of-expertise__heading">{__('Areas of expertise', 'caes-hub')}</h2>}
-                <span className="person-expertise-term">{__('Biochemistry', 'caes-hub')}</span>
-                <span className="person-expertise-term">{__('Cell Biology', 'caes-hub')}</span>
-                <span className="person-expertise-term">{__('Plant Biology', 'caes-hub')}</span>
+                {showHeading && (
+                    <TagName className="wp-block-heading person-areas-of-expertise__heading">
+                        {__('Areas of expertise', 'caes-hub')}
+                    </TagName>
+                )}
+                <div className="person-areas-of-expertise__terms">
+                    {['Biochemistry', 'Cell Biology', 'Plant Biology'].map((term) => (
+                        <span
+                            key={term}
+                            className="person-expertise-term has-hedges-background-color has-background"
+                            style={termFontSize ? { fontSize: termFontSize } : {}}
+                        >
+                            {term}
+                        </span>
+                    ))}
+                </div>
             </div>
         </>
     );
