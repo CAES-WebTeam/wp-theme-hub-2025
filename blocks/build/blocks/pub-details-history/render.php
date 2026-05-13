d91dda917d5b12f5c60f805b4c665ff0ae709e3a
Ashley A Williams <aaw97657@Ashleys-MacBook-Pro.local>
2026-05-13 15:15:07 -0400

    Migrate pub-details-history block off ACF wrapper to WP native render

--- blocks/build/blocks/pub-details-history/render.php
+++ blocks/build/blocks/pub-details-history/render.php
@@ -4,7 +4,7 @@
 $post_id = get_the_ID();
 
 // Attributes for wrapper
-$attrs = $is_preview ? ' ' : get_block_wrapper_attributes();
+$attrs = get_block_wrapper_attributes();
 
 // Get the type from ACF fields
 $history = get_field('history', $post_id);
